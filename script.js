369ea5c737dae01aca3b5ac22f0cf4badef1a46c
Ziad AK <ziadakgit@gmail.com>
2024-07-04 18:55:40 +0300

    added canvas to the form for signing options

--- script.js
+++ script.js
@@ -3,13 +3,9 @@ var ctx = canvas.getContext("2d");
 var currentMousePos = { x: -1, y: -1 };
 
 $(canvas).mousemove(function (event) {
-	currentMousePos.x = event.clientX;
-	currentMousePos.y = event.clientY;
 	ctx.fillStyle = "black";
-	ctx.fillRect(currentMousePos.x - 20, currentMousePos.y - 10, 10, 10);
-});
-
-$(document).ready(function (event) {
-	console.log(`The width is: ${canvas.style.width}`);
-    console.log(`The height is: ${canvas.style.height}`);
+	const rect = canvas.getBoundingClientRect();
+	var x = event.clientX - rect.left;
+	var y = event.clientY - rect.top;
+	ctx.fillRect(x, y, 5, 5);
 });
